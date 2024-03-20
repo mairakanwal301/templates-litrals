fd9075dfc69cd573a0b15d33fde273978caf7f9b
mairarafiq301 <mairarafiq301@gmail.com>
2024-03-20 14:25:39 -0700

    push to github

---
+++ app.js
@@ -0,0 +1,5 @@
+"use strict";
+let my_name = "Maira Kanwal";
+let message = " hellow";
+let greeting = (`${message} , how are you  ${my_name}`);
+console.log(greeting);
---
+++ app.ts
@@ -0,0 +1,6 @@
+let my_name : string = "Maira Kanwal" ;
+ let message: string = " hellow" ;
+
+ let greeting =  (`${message} , how are you  ${my_name}`) ;
+
+ console.log(greeting);
